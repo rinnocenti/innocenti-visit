efeea4f5ba55ac90595fd0c295a7d674095bfeab
Renato innocenti <renato.innocenti@gmail.com>
2020-10-10 02:00:37 -0300

    Ajustes de Modulo

--- main.js
+++ main.js
@@ -4,15 +4,12 @@ Hooks.once("init", () => {
     game.socket.on(`module.innocenti-visit`, (data) => {
         let actor = game.actors.entities.find(a => a.name === data.token);
         if (!actor) return ui.notifications.error(`Permission: Actor of ${data.token} not found`);
-        console.log(actor);
         let newpermissions = duplicate(actor.data.permission);
-        console.log(newpermissions);
         newpermissions[`${data.userid}`] = 2;
-        console.log(newpermissions);
         let permissions = new PermissionControl(actor);
         permissions._updateObject(event, newpermissions);
     });
 });
-window.InnocentiVisit = {
+window.InnocentiVisitar = {
     Visitar: Visitar
-};
+}
