9c844ee396d8154238695ef3b739a43701f391dc
Renato Innocenti <renato.innocenti@gmail.com>
2021-01-14 17:09:38 -0300

    v1.2.0

--- scripts/visitar.js
+++ scripts/visitar.js
@@ -12,22 +12,24 @@ export let Visitar = async function () {
         if (flag === 'Merchant') {
             if (!game.user.isGM) {
                 let targactor = await game.actors.entities.find(a => a.id === targetToken.actor.id);
+                let permission = false;
                 let perm = targactor.data.permission;
                 if (!perm[`${game.user.id}`]) {
-                    await game.socket.emit("module.innocenti-visit", { token: targactor.name, userid: game.user.id });
-                    console.log("Registrando log");
+                    permission = true;
                 }
+                await game.socket.emit("module.innocenti-visit", { actorName: targactor.name, userid: game.user.id, targetName: targetToken.name, permission: permission, speaker: ChatMessage.getSpeaker()});
             }
             let img = targetToken.actor.img || targetToken.data.img;
             let imgtk = targetToken.data.img || targetToken.actor.img;
             await ChatMessage.create({
-                content: `<h3><img src=\"${img}\" width=\"50px\" /> Bem Vindo à @Actor[${targetToken.actor.data._id}]{${targetToken.name}}</h3>`,
+                content: `<h3><img src=\"${img}\" width=\"50px\" /> Bem Vindo ao <strong>${targetToken.name}</strong></h3>`,
                 type: CONST.CHAT_MESSAGE_TYPES.EMOTE,
                 speaker: ChatMessage.getSpeaker(),
                 flavor: `<h3><img src=\"${imgtk}\" width=\"30px\" /></h3>`
             });
         }
+        setTimeout(function () { targetToken._onClickLeft2() }, 500);
     }
-    setTimeout(function () { targetToken._onClickLeft2() }, 500);
+    
     //await targetToken._onClickLeft2();
 }
